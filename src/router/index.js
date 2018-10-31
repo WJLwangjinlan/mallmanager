3808bc32033e39683ecb509eec8f6d5793c219f2
WJLwangjinlan <18310689172@163.com>
2018-10-31 17:50:02 +0800

    登录渲染

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/views/login'
-
 Vue.use(Router)
-
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      redirect: {name:'login'}
-    },{
-      name: 'login',
-      path: '/login',
-      redirect: {name:'login'}
-    },
+  routes: [{
+    name: 'login',
+    path: '/login',
+    component: Login
+  }
   ]
 })
